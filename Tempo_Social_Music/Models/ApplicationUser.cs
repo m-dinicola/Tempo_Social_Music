c7e1bf43db332b9e57b99bdb9c7f9589fc2497f3
mdinicola <the.m.dinicola@gmail.com>
2021-09-08 22:36:03 -0400

    Commit 0

---
+++ Tempo_Social_Music/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tempo_Social_Music.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
