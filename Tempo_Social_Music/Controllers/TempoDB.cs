fe02edcf95627351a5a49909e57eb699086c6473
anne <amlovell2@outlook.com>
2021-09-16 19:58:20 -0400

    Anne

--- Tempo_Social_Music/Controllers/TempoDB.cs
+++ Tempo_Social_Music/Controllers/TempoDB.cs
@@ -33,16 +33,28 @@ namespace Tempo_Social_Music.Controllers
         }
 
         // GET: api/TempoDB/userID
-        [HttpGet("userID/{id}")]
-        public async Task<ActionResult<TempoUser>> GetUser(int userID) {
-            var getUser = await _context.TempoUser.FirstOrDefaultAsync(x => x.UserPk == userID);
-            if(getUser is null)
-            {
-                return NotFound();
-            }
-            return getUser;
-        }
+        //[HttpGet("userID/{id}")]
+        //public async Task<ActionResult<TempoUser>> GetUser(int userID) {
+        //    var getUser = await _context.TempoUser.FirstOrDefaultAsync(x => x.UserPk == userID);
+        //    if(getUser is null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return getUser;
+        //}
 
         #endregion
+        #region Create
+        //POST: api/TempoDB/username
+        [HttpPost("user")]
+        public async Task<ActionResult<TempoUser>> CreateUser(TempoUser newUser)
+        {
+            if (GetUserByName(newUser.LoginName) != null)
+            {
+                return BadRequest();
+            }
+            return NotFound();
+        }
+               #endregion
     }
 }
