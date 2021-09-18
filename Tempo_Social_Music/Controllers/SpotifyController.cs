c49e6415ffdb30e3deaa3807e0a1fffe8f548f80
anne <amlovell2@outlook.com>
2021-09-18 15:38:31 -0400

    get spotify songId

---
+++ Tempo_Social_Music/Controllers/SpotifyController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Tempo_Social_Music.Models;
+
+namespace Tempo_Social_Music.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SpotifyController : ControllerBase
+    {
+        private SpotifyClientAuth _token;
+        private SpotifyDAL _DAL;
+        public SpotifyController(SpotifyClientAuth spotifyClientAuth)
+        {
+            _token = spotifyClientAuth;
+            _DAL = new SpotifyDAL();
+
+        }
+
+
+        //GET: /getsongbyID/{songId)
+        [HttpGet("getsongbyId/{songId}")]
+        public async Task<ActionResult<string>> GetSongById(string songId)
+        {
+            HttpClient http = _DAL.GetHttpClient();
+            http.DefaultRequestHeaders.Add("Authorization", $"Bearer {_token.GetTokenAsync().Result.access_token}");
+
+            var request = await http.GetAsync($"tracks/{songId}");
+            var response = await request.Content.ReadAsStringAsync();
+            return response;
+        }
+
+    }
+}
---
+++ Tempo_Social_Music/Models/SpotifyDAL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Tempo_Social_Music.Models
+{
+    public class SpotifyDAL
+    {
+        public HttpClient GetHttpClient()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://api.spotify.com/v1/");
+            return client;
+        }
+    }
+}
