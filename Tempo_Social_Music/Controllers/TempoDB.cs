114f8274edf76acb0d50d4ba8b9f7123de6afa88
mdinicola <the.m.dinicola@gmail.com>
2021-09-18 13:23:13 -0400

    addUserFriend and getConnections endpoints done

--- Tempo_Social_Music/Controllers/TempoDB.cs
+++ Tempo_Social_Music/Controllers/TempoDB.cs
@@ -25,7 +25,7 @@ namespace Tempo_Social_Music.Controllers
         [HttpGet("username/{username}")]
         public async Task<ActionResult<TempoUser>> GetUserByName(string username) {
             var getUser = await _context.TempoUser.FirstAsync(x => x.LoginName.ToLower() == username.ToLower());
-            if(getUser is null)
+            if (getUser is null)
             {
                 return NotFound();
             }
@@ -42,6 +42,15 @@ namespace Tempo_Social_Music.Controllers
         //    }
         //    return getUser;
         //}
+
+        // GET: api/TempoDB/Connections/{userPK}
+        // pair programmed by M & AL
+        [HttpGet("connections/{userPK}")]
+        public List<Connection> GetConnections(int userPK)
+        {
+            //return filtered table of connections which have userPK as User1 or User2 from tempoDB.Connection table
+            return _context.Connection.Where(x => x.User1 == userPK || x.User2 == userPK).ToList();
+        }
 
         #endregion
         #region Create
@@ -71,20 +80,39 @@ namespace Tempo_Social_Music.Controllers
 
         //POST: api/TempoDB/addUserFriend
         //pair progreammed by AL & MD
-        //[HttpPost ("addUserFriend/{userString}")] //Commented out due to build error. Needs to be fixed please.
-        //public async Task<ActionResult<TempoUser>> AddConnection(string userString)
-        //{
-        //    List<string> users = userString.Split('&').ToList();
-        //    if (users.Count != 2 || _context.TempoUser.Select(x => x.LoginName).Intersect(users).Count() != 2)
-        //    {
-        //        return BadRequest();
-        //    }
-        //    List<int> usernums = new List<int>();
-        //    usernums.Add( _context.TempoUser.FirstAsync(x => x.LoginName == users[0]).Result.UserPk);
-        //    usernums.Add( _context.TempoUser.FirstAsync(x => x.LoginName == users[1]).Result.UserPk);
-        //    if (_context.Connection.Where(x => usernums.(x.User1, x.User2); 
-        //    return NotFound();
-        //}
+        [HttpPost("addUserFriend/{userString}")] //Commented out due to build error. Needs to be fixed please.
+        public async Task<ActionResult<Connection>> AddConnection(string userString)
+        {
+            List<string> users = userString.Split('&').ToList();
+            if (users.Count != 2 || _context.TempoUser.Select(x => x.LoginName).Intersect(users).Count() != 2)
+            {
+                return BadRequest();
+            }
+            
+            Connection newConnection = new Connection();
+            newConnection.MatchValue = 1;
+            newConnection.User1 = _context.TempoUser.FirstAsync(x => x.LoginName == users[0]).Result.UserPk;
+            newConnection.User2 = _context.TempoUser.FirstAsync(x => x.LoginName == users[1]).Result.UserPk;
+
+            Connection oldConnection = await _context.Connection.FirstOrDefaultAsync(x =>
+                (x.User1 == newConnection.User1 && x.User2 == newConnection.User2) ||
+                (x.User1 == newConnection.User2 && x.User2 == newConnection.User1)
+                );
+            if (oldConnection is null)
+            {
+                _context.Connection.Add(newConnection);
+                await _context.SaveChangesAsync();
+                return CreatedAtAction(nameof(GetConnections), new { userPK = newConnection.User1}, newConnection);
+            }
+            return Ok(oldConnection);
+        }
+
+        #endregion
+        #region Delete
+        //DELETE: api/tempoDB/deleteUserFriend/{userString}
+        //pair programmed by M and AL
+
+
         #endregion
     }
 }
